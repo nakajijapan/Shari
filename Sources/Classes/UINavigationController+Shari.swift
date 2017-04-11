fe57b26534b6d29fe5795d672a9d48a2313967ba
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2017-04-11 11:41:49 +0900

    call viewWillAppear and viewDidAppear

--- Sources/Classes/UINavigationController+Shari.swift
+++ Sources/Classes/UINavigationController+Shari.swift
@@ -27,7 +27,7 @@ public extension Shari where Base: UINavigationController {
             viewControllerToPresent.endAppearanceTransition()
             viewControllerToPresent.didMove(toParentViewController: strongSelf.base)
         }
-        
+
         let tapGestureRecognizer = UITapGestureRecognizer(
             target: base,
             action: #selector(base.overlayViewDidTap(_:))
@@ -44,46 +44,56 @@ public extension Shari where Base: UINavigationController {
         (sourceViewController, destinationViewController) = bothEndsViewControllers(
             viewController: base.visibleViewController,
             childViewControllers: base.childViewControllers)
-        
+
         sourceViewController?.willMove(toParentViewController: nil)
         base.willMove(toParentViewController: nil)
+
+        sourceViewController?.beginAppearanceTransition(false, animated: true)
         destinationViewController?.beginAppearanceTransition(true, animated: true)
 
         ModalAnimator.dismiss(
             fromView: parentTargetView,
             presentingViewController: base.visibleViewController) {
                 completion?()
-                sourceViewController?.removeFromParentViewController()
+
+                sourceViewController?.endAppearanceTransition()
                 destinationViewController?.endAppearanceTransition()
+
+                sourceViewController?.removeFromParentViewController()
         }
-        
+
     }
 
 }
 
 extension UINavigationController {
-    
+
     @objc fileprivate func overlayViewDidTap(_ gestureRecognizer: UITapGestureRecognizer) {
-        
+
         weak var sourceViewController: UIViewController?
         weak var destinationViewController: UIViewController?
         (sourceViewController, destinationViewController) = si.bothEndsViewControllers(
             viewController: childViewControllers.last,
             childViewControllers: childViewControllers)
-        
+
         si.parentTargetView.isUserInteractionEnabled = false
 
         sourceViewController?.willMove(toParentViewController: nil)
         willMove(toParentViewController: nil)
+
+        sourceViewController?.beginAppearanceTransition(false, animated: true)
         destinationViewController?.beginAppearanceTransition(true, animated: true)
 
         ModalAnimator.dismiss(
             fromView: si.parentTargetView,
             presentingViewController: visibleViewController) { [weak self] in
-                sourceViewController?.removeFromParentViewController()
+
+                sourceViewController?.endAppearanceTransition()
                 destinationViewController?.endAppearanceTransition()
+
+                sourceViewController?.removeFromParentViewController()
                 self?.si.parentTargetView.isUserInteractionEnabled = true
         }
-        
+
     }
 }
--- Sources/Classes/UITabBarController+Shari.swift
+++ Sources/Classes/UITabBarController+Shari.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 public extension Shari where Base: UITabBarController {
-    
+
     var parentTargetView: UIView {
         return base.view
     }
-    
+
     func present(_ viewControllerToPresent: UIViewController) {
-        
+
         base.addChildViewController(viewControllerToPresent)
         viewControllerToPresent.beginAppearanceTransition(true, animated: true)
         ModalAnimator.present(toView: viewControllerToPresent.view, fromView: parentTargetView) { [weak self] in
@@ -23,14 +23,14 @@ public extension Shari where Base: UITabBarController {
             viewControllerToPresent.endAppearanceTransition()
             viewControllerToPresent.didMove(toParentViewController: strongslef.base)
         }
-        
+
         let tapGestureRecognizer = UITapGestureRecognizer(
             target: base,
             action: #selector(base.overlayViewDidTap(_:))
         )
         let overlayView = ModalAnimator.overlayView(fromView: parentTargetView)
         overlayView!.addGestureRecognizer(tapGestureRecognizer)
-        
+
     }
     
     func dismiss(completion: (() -> Void)? = nil) {
@@ -43,16 +43,21 @@ public extension Shari where Base: UITabBarController {
 
         sourceViewController?.willMove(toParentViewController: nil)
         base.willMove(toParentViewController: nil)
+
+        sourceViewController?.beginAppearanceTransition(false, animated: true)
         destinationViewController?.beginAppearanceTransition(true, animated: true)
 
         ModalAnimator.dismiss(
             fromView: parentTargetView,
             presentingViewController: sourceViewController) {
                 completion?()
-                sourceViewController?.removeFromParentViewController()
+
+                sourceViewController?.endAppearanceTransition()
                 destinationViewController?.endAppearanceTransition()
+
+                sourceViewController?.removeFromParentViewController()
         }
-        
+
     }
 
 }
@@ -60,7 +65,7 @@ public extension Shari where Base: UITabBarController {
 extension UITabBarController {
     
     @objc fileprivate func overlayViewDidTap(_ gestureRecognizer: UITapGestureRecognizer) {
-      
+        
         weak var sourceViewController: UIViewController?
         weak var destinationViewController: UIViewController?
         (sourceViewController, destinationViewController) = si.bothEndsViewControllers(
@@ -71,14 +76,17 @@ extension UITabBarController {
 
         sourceViewController?.willMove(toParentViewController: nil)
         willMove(toParentViewController: nil)
+        sourceViewController?.beginAppearanceTransition(false, animated: true)
         destinationViewController?.beginAppearanceTransition(true, animated: true)
         
         ModalAnimator.dismiss(
             fromView: si.parentTargetView,
             presentingViewController: sourceViewController) { [weak self] in
 
-                sourceViewController?.removeFromParentViewController()
+                sourceViewController?.endAppearanceTransition()
                 destinationViewController?.endAppearanceTransition()
+
+                sourceViewController?.removeFromParentViewController()
                 self?.si.parentTargetView.isUserInteractionEnabled = true
         }
         

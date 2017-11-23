55b1ee61ad55f1dce46a60b5d09522201ec3a5e6
nakajijapan <daichi-nakajima@cookpad.com>
2017-11-23 21:59:26 +0900

    Add Test for map

--- ShariTests/ShariTests.swift
+++ ShariTests/ShariTests.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 import XCTest
-import Shari
+@testable import Shari
 
 class ShariTests: XCTestCase {
     
     func testSetting() {
-        //XCTAssertEqual(Shari.BackgroundColorOfOverlayView, UIColor(red: 0, green: 0, blue: 0, alpha: 1.0))
-        //XCTAssertEqual(Shari.ShouldTransformScaleDown, true)
+        XCTAssertEqual(ShariSettings.backgroundColorOfOverlayView, UIColor(red: 0, green: 0, blue: 0, alpha: 1.0))
+        XCTAssertEqual(ShariSettings.shouldTransformScaleDown, true)
     }
-    
+
+
+    func testMap() {
+
+        var value = ModalAnimator.map(value: 400, inMin: 0, inMax: 800, outMin: 0.0, outMax: 1.0)
+        XCTAssertEqual(value, 0.5)
+
+        value = ModalAnimator.map(value: 400, inMin: 0, inMax: 800, outMin: 0.5, outMax: 1.0)
+        XCTAssertEqual(value, 0.75)
+
+        value = ModalAnimator.map(value: 450, inMin: 0, inMax: 800, outMin: 0.9, outMax: 1.0)
+        XCTAssertEqual(value, 0.95625)
+
+        value = ModalAnimator.map(value: 450, inMin: 0, inMax: 812, outMin: 0.7757, outMax: 1.0)
+        XCTAssertGreaterThan(value, 0.9)
+    }
+
 }

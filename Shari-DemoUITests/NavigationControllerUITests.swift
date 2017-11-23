104bd3478a98f329e27a1c876b580f1868471403
nakajijapan <daichi-nakajima@cookpad.com>
2017-11-23 21:59:11 +0900

    Failed because it could not tap the backgroundview

--- Shari-DemoUITests/NavigationControllerUITests.swift
+++ Shari-DemoUITests/NavigationControllerUITests.swift
@@ -15,7 +15,6 @@ class NavigationControllerUITests: XCTestCase {
         
         continueAfterFailure = false
         XCUIApplication().launch()
-        
     }
     
     override func tearDown() {
@@ -23,31 +22,25 @@ class NavigationControllerUITests: XCTestCase {
     }
     
     func testShowAndClose() {
-        
         let app = XCUIApplication()
         app.tables.staticTexts["Detail View Controller"].tap()
         app.buttons["Button"].tap()
         app.navigationBars["modal"].buttons["Close"].tap()
-        
     }
     
     func testShowAndSelect() {
-        
         let app = XCUIApplication()
         let tablesQuery = app.tables
         tablesQuery.staticTexts["Detail View Controller"].tap()
         app.buttons["Button"].tap()
         tablesQuery.staticTexts["Title #0"].tap()
-        
     }
     
     func testCloseWhenTappingBackgroundView() {
-        
         let app = XCUIApplication()
         app.tables.staticTexts["Detail View Controller"].tap()
         app.buttons["Button"].tap()
-        app.otherElements.containing(.navigationBar, identifier:"Shari.Navigation").children(matching: .other).element(boundBy: 1).children(matching: .image).element.tap()
-        
+        app.otherElements["ShariOverlayView"].tap()
     }
 
 }

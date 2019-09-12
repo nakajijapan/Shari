ec2133368c8ecdbfd08a3d17ff90832a39b9b6cc
nakajijapan <pp.kupepo.gattyanmo@gmail.com>
2019-09-12 14:21:16 +0900

    Delete space

--- Shari-Demo/DetailViewController.swift
+++ Shari-Demo/DetailViewController.swift
@@ -27,22 +27,22 @@ class DetailViewController: UIViewController {
         //ShariSettings.shouldTransformScaleDown = false
         //ShariSettings.backgroundColorOfOverlayView = UIColor(red: 1, green: 0, blue: 0, alpha: 0.6)
         //ShariSettings.isUsingScreenShotImage = false
-        
+
         modalNavigationController.parentNavigationController = navigationController
         modalNavigationController.cornerRadius = 8
         //modalNavigationController.visibleHeight = 200 // Default: UIScreen.main.bounds.height * 0.5
         navigationController?.si.present(modalNavigationController)
     }
-    
+
     @IBAction func button2DidTap(button: UIButton) {
         guard let modalNavigationController = storyboard!.instantiateViewController(withIdentifier: "ModalV2NavigationController") as? ShariNavigationController else {
             fatalError("Need the ShariNavigationController")
         }
-        
+
         // Transition Setting
         ShariSettings.backgroundColorOfOverlayView = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
         ShariSettings.isUsingScreenShotImage = false
-        
+
         modalNavigationController.parentNavigationController = navigationController
         modalNavigationController.cornerRadius = 8
         navigationController?.si.present(modalNavigationController)

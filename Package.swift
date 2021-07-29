9ea5694a9b3807a970fa1378ace06786fbd3227d
nakajijapan <daichi.nakajima@dena.com>
2021-07-29 16:49:28 +0900

    Create Package file

---
+++ Package.swift
@@ -0,0 +1,19 @@
+
+// swift-tools-version:5.4.0
+import PackageDescription
+
+let package = Package(
+    name: "Shari",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "Shari", targets: ["Shari"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Shari",
+            dependencies: [],
+            path: "Sources"
+        )
+    ]
+)
